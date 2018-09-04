46717f7cc278296b0180979fc9ebc1f20b55d12b
ChattyRS <rschatty@hotmail.com>
2018-09-04 22:28:20 +0200

    Removed Ithell location

--- cogs/updateLocs.py
+++ cogs/updateLocs.py
@@ -19,9 +19,9 @@ client = gspread.authorize(creds)
 
 sheet = client.open(config['sheetName']).sheet1
 
-locations = ["CA", "LC", "BA", "SP", "BU", "CW", "PRIF", "MG", "VIP", "GE", "ITH", "MEI", "TRA"]
+locations = ["CA", "LC", "BA", "SP", "BU", "CW", "PRIF", "MG", "VIP", "GE", "MEI", "TRA"]
 portablesNames = ['Fletcher', 'Crafter', 'Brazier', 'Sawmill', 'Forge', 'Range', 'Well']
-locs = 'CA, LC, BA, SP, BU, CW, PRIF, MG, VIP, GE, ITH, MEI, TRA'
+locs = 'CA, LC, BA, SP, BU, CW, PRIF, MG, VIP, GE, MEI, TRA'
 busyLocs = [[84, "CA"], [99, "CA"], [100, "SP"]]
 forbiddenLocs = [[2, "BU"]]
 highestWorld = 141
@@ -188,7 +188,7 @@ def format(ports):
     if not txt:
         return 'N/A'
 
-    txt = txt.replace('PRIF', 'Prif').replace('ITH', 'Ithell').replace('MEI', 'Meilyr').replace('TRA', 'Trahaearn')
+    txt = txt.replace('PRIF', 'Prif').replace('MEI', 'Meilyr').replace('TRA', 'Trahaearn')
 
     return txt
 
