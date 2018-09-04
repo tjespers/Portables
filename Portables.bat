144df33c426137e6b68665aefcc5adcabd4af6b5
ChattyRS <rschatty@hotmail.com>
2018-09-04 22:29:02 +0200

    Removed deprecated code

--- Portables.bat
+++ Portables.bat
@@ -1,12 +1,3 @@
-call getCmdPID
-set "current_pid=%errorlevel%"
-
-for /f "skip=3 tokens=2 delims= " %%a in ('tasklist /fi "imagename eq cmd.exe"') do (
-    if "%%a" neq "%current_pid%" (
-        TASKKILL /PID %%a /f >nul 2>nul
-    )
-)
-
 cd %~dp0
 :loop
 python main.py
