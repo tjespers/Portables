838230f2cfb1d00be889fabde330b05ec850c9f0
ChattyRS <rschatty@hotmail.com>
2018-09-10 21:58:39 +0200

    Added -sheetactivity command

--- cogs/adminSheets.py
+++ cogs/adminSheets.py
@@ -47,6 +47,7 @@ class adminSheets:
         '''
         Notes rank activity on admin sheets (Admin+).
         '''
+        await self.bot.send_typing(ctx.message.channel)
         msg = ctx.message
         user = msg.author
         roles = user.roles
@@ -66,7 +67,7 @@ class adminSheets:
         if not rankNames:
             await self.bot.say('Please add the rank(s) for whom you want to note activity on the sheets as argument(s).')
             return
-        month datetime.utcnow().strftime("%B")
+        month = datetime.utcnow().strftime("%B")
         try:
             sheetMonth = sheet.cell(3, 1).value
         except:
@@ -114,6 +115,91 @@ class adminSheets:
                 sheet.update_cell(row, col, timestamp)
             await self.bot.say(f'**{name}** has been noted as active for **{timestamp}** **{datetime.utcnow().strftime("%b")}**.')
 
+    @commands.command(pass_context=True)
+    async def sheetactivity(self, ctx, *rankNames):
+        '''
+        Notes rank sheet activity on admin sheets (Admin+).
+        '''
+        await self.bot.send_typing(ctx.message.channel)
+        msg = ctx.message
+        user = msg.author
+        roles = user.roles
+        channel = msg.channel
+        server = msg.server
+        if server != self.server:
+            await self.bot.say('Sorry, this command can only be used in the Portables Discord server.')
+            return
+        isAdmin = False
+        for r in roles:
+            if r.id == self.admin.id or user.id == config['owner']:
+                isAdmin = True
+                break
+        if not isAdmin:
+            await self.bot.say('Sorry, this command can only be used by Admins.')
+            return
+        if not rankNames:
+            await self.bot.say('Please add the rank(s) for whom you want to note activity on the sheets as argument(s).')
+            return
+        month = datetime.utcnow().strftime("%B")
+        try:
+            sheetMonth = sheet.cell(3, 1).value
+        except:
+            regen()
+            sheetMonth = sheet.cell(3, 1).value
+        if month != sheetMonth:
+            await self.bot.say(f'Sorry, the admin sheets have not been updated to the current month yet. Please wait for a Leader to finish doing upkeep.')
+            return
+        try:
+            ranks = sheet.col_values(1)[headerRows:]
+        except:
+            regen()
+            ranks = sheet.col_values(1)[headerRows:]
+        for i, rank in enumerate(ranks):
+            if rank is None or not rank:
+                ranks = ranks[:i]
+                break
+        timestamp = datetime.utcnow().strftime("%#d")
+        for name in rankNames:
+            row = 0
+            for i, rank in enumerate(ranks):
+                if rank in rankTitles:
+                    continue
+                if name.upper() in rank.upper():
+                    row = i + headerRows + 1
+                    name = rank
+                    break
+            if not row:
+                await self.bot.say(f'Sorry, I could not find a rank by the name **{name}**.')
+                continue
+            try:
+                activity = sheet.row_values(row)
+                sheetActivity = activity[34:65]
+                activity = activity[3:34]
+            except:
+                regen()
+                activity = sheet.row_values(row)
+                sheetActivity = activity[34:65]
+                activity = activity[3:34]
+            sheetActivity = list(filter(bool, sheetActivity))
+            activity = list(filter(bool, activity))
+            if timestamp in sheetActivity:
+                await self.bot.say(f'**{name}** has already been noted as active on sheets for today.')
+                continue
+            active = False
+            if timestamp in activity:
+                active = True
+            activityCol = 4 + len(activity)
+            sheetCol = 35 + len(sheetActivity)
+            try:
+                sheet.update_cell(row, sheetCol, timestamp)
+                if not active:
+                    sheet.update_cell(row, activityCol, timestamp)
+            except:
+                regen()
+                sheet.update_cell(row, sheetCol, timestamp)
+                if not active:
+                    sheet.update_cell(row, activityCol, timestamp)
+            await self.bot.say(f'**{name}** has been noted as active on sheets for **{timestamp}** **{datetime.utcnow().strftime("%b")}**.')
 
 def setup(bot):
     bot.add_cog(adminSheets(bot))
